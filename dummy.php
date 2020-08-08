6408a12dc84347183cbb0a658b9bc6199d8f82b1
itsmerajesh17 <69048956+itsmerajesh17@users.noreply.github.com>
2020-08-08 22:43:09 +0530

    new dummy
    Co-Authored-By: itsmespd <69047724+itsmespd@users.noreply.github.com>

--- dummy.php
+++ dummy.php
@@ -4,11 +4,44 @@
 	// $rest_json = file_get_contents("php://input");
 	// $_POST = json_decode($rest_json, true);
 
-    header('Access-Control-Allow-Origin: *');
-    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-    header("Access-Control-Allow-Headers: Content-Disposition, Content-Type, Content-Length, Accept-Encoding");
-    header("Content-type:application/json");
+    // header('Access-Control-Allow-Origin: *');
+    // header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    // header("Access-Control-Allow-Headers: Content-Disposition, Content-Type, Content-Length, Accept-Encoding");
+    // header("Content-type:application/json");
 
+ //    header('Access-Control-Allow-Origin: *');
+
+	// header('Access-Control-Allow-Methods: GET, POST');
+
+	// header("Access-Control-Allow-Headers: X-Requested-With");
+	// header('Access-Control-Max-Age: 86400');
+
+	function cors() {
+
+    // Allow from any origin
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+        // you want to allow, and if so:
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    }
+
+    // Access-Control headers are received during OPTIONS requests
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            // may also be using PUT, PATCH, HEAD etc
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+        exit(0);
+    }
+
+    echo "You have CORS!";
+}
 
 //     header('Content-Type: application/json; charset=utf-8');
 // header("Access-Control-Allow-Origin: *");
